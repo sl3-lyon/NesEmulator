34667fda5e939b1c9df45597aefaa3e828950f3a
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2015-01-02 16:53:59 +0100

    Update RomHeader.h

--- src/core/RomHeader.h
+++ src/core/RomHeader.h
@@ -1,11 +1,15 @@
 #ifndef NESEMU_ROMHEADER
 #define NESEMU_ROMHEADER
 
+#include <array>  // std::array
+#include <vector> // std::vector
+
 enum TvSystem { Ntsc, Pal };
 
 struct RomHeader
 {
     std::vector<int> bits;
+    std::array<int, 1fff> sram;
     //int PrgRomPagesCount;
     //int ChrRomPagesCount;
 };
