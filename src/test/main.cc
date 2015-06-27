e935c850ba6cc75502654d1dd45684edf8ceb195
vincent <vincent.penando@hotmail.fr>
2015-06-27 13:14:21 +0000

    ajout lua local

--- src/test/main.cc
+++ src/test/main.cc
@@ -1,5 +1,10 @@
 #include "noncopyable.h"
 #include "cpu.h"
 
+#include <lua.hpp>
+
 int main() {
+	lua_State *L = luaL_newstate(); //lua_open();
+	luaL_openlibs(L);
+	luaL_dostring(L, "print('lol')");
 }
